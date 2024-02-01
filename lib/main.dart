51a7253ce0388a660baaee9f19cd7492635f759b
Natanael Hausmano <natanaelhlacerda@hotmail.com>
2024-02-01 12:40:57 -0300

    correção person_list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:uap_app/core/person_list/view/person_list_view.dart';
+import 'package:uap_app/features/person_list/view/person_list_view.dart';
 import 'package:uap_app/features/login/view/login_view.dart';
 import 'package:uap_app/features/person_map/presentation/ui/person_map_view.dart';
 import 'package:uap_app/features/register/view/register_view.dart';
